480261fb85bbe13e7a3eeb6e80f41fe5694362b5
sbranda <brandaster@gmail.com>
2026-07-06 13:26:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = 'subtivoz-v2';
+const CACHE_NAME = 'subtivoz-v3';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => console.error('No se pudo precachear todo, pero el SW igual se instala:', err))
   );
   self.skipWaiting();
 });
